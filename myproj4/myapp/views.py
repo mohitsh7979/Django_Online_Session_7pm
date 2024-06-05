cb3321316f0e99888142bef5918991d6ae0f7766
mohitsh7979 <mohitsh7979@gmail.com>
2024-06-05 20:43:35 +0530

    Update

--- myproj4/myapp/views.py
+++ myproj4/myapp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from .models import Student
 # Create your views here.
 
 def index(request):
     data = Student.objects.all()
-    return render(request,'index.html',{'data':data})
+    return render(request,'Table.html',{'data':data})
+
+def getForm(request):
+    if request.method == "POST":
+        name = request.POST['uname']
+        email = request.POST['uemail']
+        address = request.POST['address']
+        mobile = request.POST['mobile']
+        date = request.POST['date']
+        age = request.POST['age']
+        Student(name=name,email=email,age=age,address = address,mobile_no=mobile,date=date).save()
+        return redirect('/')
+    return render(request,'Form.html')
--- myproj4/myproj4/urls.py
+++ myproj4/myproj4/urls.py
@@ -20,5 +20,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index),
+    path('form/',views.getForm)
 ]
